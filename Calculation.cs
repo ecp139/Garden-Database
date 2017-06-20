a1e148f42817c5fa04d090978bbe5369f2403369
Esra Petursson <ecp139@gmail.com>
2017-06-20 11:39:29 -0400

    Garden Database

---
+++ Calculation.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Mono.Data.Sqlite;
+using System.Data;
+
+namespace Garden
+{
+	public class Calculation
+	{
+		
+		SqliteConnection connection;
+		SqliteCommand command; //defining command variable
+		SqliteDataReader reader; //
+
+		public void openDataConnection()
+		{
+
+			connection = new SqliteConnection(@"Data Source =/Users/esrapetursson/Desktop/Garden/Garden/garden.sqlite");
+			connection.Open();
+		}
+
+		public void closeDataConnection()
+		{
+			connection = new SqliteConnection(@"Data Source =/Users/esrapetursson/Desktop/Garden/Garden/garden.sqlite");
+			connection.Close();
+		}
+		public void display_table()
+		{
+			openDataConnection();
+			SqliteCommand show_info = new SqliteCommand("SELECT * FROM garden", connection);
+			SqliteDataReader display = show_info.ExecuteReader();
+			while (display.Read())
+			{
+				Console.Write(display["plant"] + ": ");
+				Console.WriteLine(display["per_sq"] + " plants per sq ft");
+			}
+			closeDataConnection();
+		}
+
+		public void plant_choice(double area)
+		{
+			
+			openDataConnection();
+
+			string answer = " ";
+			answer = Console.ReadLine().ToLower();
+
+			if (answer == "basil" || answer == "radishes" || answer == "sweet potatoes")
+			{
+				command = new SqliteCommand($"SELECT * FROM garden WHERE plant = '{answer}'", connection);
+				reader = command.ExecuteReader();
+			}
+
+			else if (answer != "basil" || answer != "radishes" || answer != "sweet potatoes")
+			{
+				Console.WriteLine("That is an invalid response.");
+				return;
+			}
+
+			if (reader.HasRows) //see if rows exist in database if there's nothing in the table, this will not execute
+			{
+				while (reader.Read())
+				{
+					double plant_area = area * reader.GetInt32(reader.GetOrdinal("per_sq"));
+					plant_area = Math.Floor(plant_area);
+					Console.WriteLine("You can plant" + " " + plant_area + " " + reader["plant"] + " plants");
+				}
+			}
+
+			reader.Close();
+			closeDataConnection();
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Mono.Data.Sqlite;
+using System.Data;
+
+namespace Garden
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello what are the dimensions of your raised garden bed in feet?");
+			Console.WriteLine("What is the length of the bed?");
+			double length = Convert.ToDouble(Console.ReadLine());
+
+			Console.WriteLine("What is the width of the bed?");
+			double width = Convert.ToDouble(Console.ReadLine());
+
+			double area = length * width;
+			Console.WriteLine($"The area of your raised bed is {area} square feet.");
+			Console.WriteLine();
+
+			Calculation db = new Calculation();
+			Console.WriteLine("Plants can be planted in the following amounts.");
+			Console.WriteLine();
+
+			db.display_table();
+
+			Console.WriteLine();
+
+			string replant = "y";
+
+			while (replant == "y")
+			{
+				Console.WriteLine("Do you want to plant Basil, Radishes or Sweet Potatoes?");
+
+				db.plant_choice(area);
+
+				Console.WriteLine("Would you like to plant something else?  Type (y)es or (n)o.");
+
+				replant = Console.ReadLine().ToLower();
+
+				if (replant == "n")
+				{
+					Console.WriteLine("Have a nice day!");
+				}
+			}
+		}
+	}
+}
